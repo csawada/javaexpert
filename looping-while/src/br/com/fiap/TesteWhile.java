3a62907ae9fcd3583cba3b373a4808efcc77daed
csawada <csawada@gmail.com>
2022-01-27 20:46:13 -0300

    Aula While

---
+++ looping-while/src/br/com/fiap/TesteWhile.java
@@ -0,0 +1,16 @@
+package br.com.fiap;
+
+import java.util.Scanner;
+
+public class TesteWhile {
+
+	public static void main(String[] args) {
+		/*
+		 Looping
+		 Laço de repetição
+		 */
+		Scanner leitor = new Scanner(System.in);
+		String nome = leitor.next();
+	}
+
+}
