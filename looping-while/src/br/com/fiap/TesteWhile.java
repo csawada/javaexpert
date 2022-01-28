9fa7898b1fee63dad6b36c6ffb8f7dfd666b3f7b
csawada <csawada@gmail.com>
2022-01-27 21:01:31 -0300

    Aula2101

--- looping-while/src/br/com/fiap/TesteWhile.java
+++ looping-while/src/br/com/fiap/TesteWhile.java
@@ -10,7 +10,17 @@ public class TesteWhile {
 		 Laço de repetição
 		 */
 		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite seu nome: ");
 		String nome = leitor.next();
+		
+		int contador = 0;
+		
+		while (contador < 10) {
+			System.out.println(contador + " - " + nome);
+			contador = contador + 1;
+		}
+		
+		System.out.println("FIM");
 	}
 
 }
