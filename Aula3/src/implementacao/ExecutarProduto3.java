2f2fa4e84f066d538e2131a7690ebc2780798c55
csawada <csawada@gmail.com>
2022-02-09 21:24:02 -0300

    aula03

--- Aula3/src/implementacao/ExecutarProduto3.java
+++ Aula3/src/implementacao/ExecutarProduto3.java
@@ -1,13 +1,21 @@
 package implementacao;
 
 import beans.Produto;
+import util.Input;
+
 
 public class ExecutarProduto3 {
-
+	
 	public static void main(String[] args) {
+
+		
 		Produto p1 = new Produto();
 		
-
-	}
-
+		p1.setMarca(Input.texto("Informe a Marca"));
+		p1.setNome(Input.texto("Informe a Nome/Descrição"));
+		p1.setValor(Input.decimal("Informe o Valor"));
+		p1.setPromocao(Input.logico("Esta em promoção?"));
+		System.out.println(p1.detalhe());
+	}	
+ 
 }
